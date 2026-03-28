48f4b705ccae680a851df9f5f7aa526b26f01f6e
zPedroLuis <zpedroluis@outlook.com>
2026-03-28 05:17:52 -0300

    refactor: padroniza importação do AppDataSource nos services

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,20 +1,16 @@
 import { DataSource } from "typeorm"
 
-export const AppDataSource = new DataSource({
+
+const isCompiled = __dirname.includes('dist');
+const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
     username: "postgres",
     password: "1234",
     database: "webagenda",
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/database/migrations/*.ts"]
-})
+    entities: [isCompiled ? "dist/entities/*.js" : "src/entities/*.ts"]
+});
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
+
+export default AppDataSource;
