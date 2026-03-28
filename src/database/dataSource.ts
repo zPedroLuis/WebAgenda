0acd7c7fb78c47db02be9493ca6bd5d61bda9f53
zPedroLuis <zpedroluis@outlook.com>
2026-03-28 06:34:44 -0300

    refactor: exporta AppDataSource como nomeado e ajusta configuração para imports diretos das entities

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,16 +1,17 @@
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm";
+import { Pessoa } from "../entities/Pessoa";
+import { Evento } from "../entities/Evento";
 
-
-const isCompiled = __dirname.includes('dist');
-const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
     username: "postgres",
     password: "1234",
     database: "webagenda",
-    entities: [isCompiled ? "dist/entities/*.js" : "src/entities/*.ts"]
+    synchronize: true,
+    logging: true,
+    entities: [Pessoa, Evento],
+    migrations: [],
+    subscribers: [],
 });
-
-
-export default AppDataSource;
