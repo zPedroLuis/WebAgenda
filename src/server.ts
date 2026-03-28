2d8739817844bb82003d948d766f0685643ccdd8
zPedroLuis <zpedroluis@outlook.com>
2026-03-28 06:35:24 -0300

    refactor(server): inicializa AppDataSource corretamente e integra middlewares, rotas e Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,20 @@
-import "reflect-metadata"
-import express from "express"
-import "./database/dataSource.ts"
-import "./database"
-
-import { routes } from "./routes"
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import { AppDataSource } from "./database/dataSource";
+import { routes } from "./routes";
 const { swaggerUi, swaggerSpec } = require("./swagger");
 
-const cors = require("cors")
+const app = express();
 
-const app = express()
-
-app.use(express.json())
-
-app.use(cors())
-
-
+app.use(express.json());
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(routes)
+app.use(routes);
 
-app.listen(2999, () => console.log("Server is running"))
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source initialized");
+    app.listen(2999, () => console.log("Server is running on http://localhost:2999"));
+  })
+  .catch((err) => console.error("Error during Data Source initialization:", err));
